59208a0b69a72453fdfdd534e89c723ace521bea
Devisree-Arepalli <73283630+Devisree-Arepalli@users.noreply.github.com>
2022-01-02 17:59:41 +0530

    Create 02)OVERLAPPING-RECTANGLES.cpp

---
+++ 02)OVERLAPPING-RECTANGLES.cpp
@@ -0,0 +1,14 @@
+/*Given two rectangles, find if the given two rectangles overlap or not. A rectangle is denoted by providing the x and y coordinates of two points: */
+
+class Solution {
+  public:
+    int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+        // code here
+        if(L1[0]>R2[0] ||L2[0]>R1[0])
+          return false;
+          else if(L1[1]<R2[1] ||L2[1]<R1[1])
+            return false;
+            
+            return true;
+    }
+};

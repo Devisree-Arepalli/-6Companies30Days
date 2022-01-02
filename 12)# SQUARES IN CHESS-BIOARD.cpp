dacf441895d8095e826eaa43a4837d307b826a3f
Devisree-Arepalli <73283630+Devisree-Arepalli@users.noreply.github.com>
2022-01-02 18:40:34 +0530

    Create 12)# SQUARES IN CHESS-BIOARD.cpp

---
+++ 12)# SQUARES IN CHESS-BIOARD.cpp
@@ -0,0 +1,16 @@
+/*   Find total number of Squares in a N*N chessboard   */
+
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long N) {
+        // code here
+        long long ans=0;
+        while(N>=1)
+        {
+            ans+=N*N;
+            N--;
+        }
+        return ans;
+    }
+};
